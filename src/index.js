85977aa26f4c6c5e86c059575a81ce3f6cc58d6d
prodanserega <74560554+prodanserega@users.noreply.github.com>
2021-11-30 18:15:03 +0200

    slider

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ new Swiper('.swiper', {
   loop: true,
 
   // If we need pagination
-  //   pagination: {
-  //     el: '.swiper-pagination',
-  //   },
+  pagination: {
+    el: '.swiper-pagination',
+  },
 
   // Navigation arrows
   navigation: {
